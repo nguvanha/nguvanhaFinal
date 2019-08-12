1589fdf1ca63ebc5b8e73d3bec4a05298bc34987
nguvanha <52674146+nguvanha@users.noreply.github.com>
2019-08-12 11:55:22 -0400

    Add files via upload

---
+++ assets/data/speakerData.ts
@@ -0,0 +1,7 @@
+import { Speaker } from '../../app/speaker'
+
+export const SPEAKERDATA: Speaker[] = [
+    {style: "Tufton", color: "brown", brand: "Marshall"},
+    {style: "Soundtouch 30", color: "Silver", brand: "Bose"},
+    {style: "Dr. Dre Pill+", color: "brown", brand: "Beat"}
+]
